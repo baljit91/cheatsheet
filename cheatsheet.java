76135243f9569281d7be43d81d96ace81020ec1c
Baljit Singh <baljitdhanjal@gmail.com>
2017-06-10 11:19:45 -0400

    Create cheatsheet.java

---
+++ cheatsheet.java
@@ -0,0 +1,9 @@
+//Tree height
+
+    public int height(TreeNode root){
+        if(root == null)
+            return 0;
+            
+        return Math.max(height(root.left),height(root.right)) + 1;
+    }
+    
