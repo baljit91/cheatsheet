fab95b9810ad28c2946b4f2922f4fbce45be4137
Baljit Singh <baljitdhanjal@gmail.com>
2017-06-12 21:38:29 -0400

    Create cheatsheet.java

--- cheatsheet.java
+++ cheatsheet.java
@@ -6,4 +6,16 @@
             
         return Math.max(height(root.left),height(root.right)) + 1;
     }
+----------------------------------------------------------------------
+//linked list middle
+
+while (fast_ptr != null && fast_ptr.next != null)
+            {
+                fast_ptr = fast_ptr.next.next;
+                slow_ptr = slow_ptr.next;
+            }
+
+-----------------------------------------------------------------------
+    
+    
     
