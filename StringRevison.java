7cb3109893a64476c8b9b1251aaef939912e167c
Baljit Singh <baljitdhanjal@gmail.com>
2017-06-18 18:53:47 -0400

    Create StringRevison.java

---
+++ StringRevison.java
@@ -0,0 +1,14 @@
+//1. Longest Palindrome(mode : easy)
+
+Technique        
+1. Use HashSet.
+2. Use 2 array for lowecase and uppercase.
+
+Time Complexity:
+O(n).
+Space Complexity:
+
+O(Types of charcters the string have).
+
+
+//2.
