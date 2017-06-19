b10be02e37afe599838532432d5031ffa4bebdfa
Baljit Singh <baljitdhanjal@gmail.com>
2017-06-19 00:33:17 -0400

    Update StringRevison.java

--- StringRevison.java
+++ StringRevison.java
@@ -11,4 +11,21 @@ Space Complexity:
 O(Types of charcters the string have).
 
 
-//2.
+//2.Minimum characters to be added at front to make string palindrome
+  
+  Technique        
+1. brute force.(remove the last charcter and check the substring is palindrome and keep on increasing count if it is not
+or keep on adding the last charctert in the front of the string)
+
+
+Time Complexity:
+O(n2). in worst case
+Space Complexity:
+O(n).
+
+  2.efficiently in O(n) time using lps array of KMP algorithm.
+  
+  Time Complexity:
+  O(n). in worst case
+  Space Complexity:
+  O(n).
