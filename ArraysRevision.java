294a40c7f73b6524ec51c99598f448179e3baa7b
Baljit Singh <baljitdhanjal@gmail.com>
2017-06-20 22:15:37 -0400

    Update ArraysRevision.java

--- ArraysRevision.java
+++ ArraysRevision.java
@@ -1,1 +1,33 @@
 1. Search in Rotated Sorted Array.
+2. Binary search - finding first or last occurrence of a number.
+  
+  result = -1
+  while(low <= high){
+    int mid =(low + high)/2;
+    if(arr[mid] == target){
+        result = mid;
+        high = mid -1;//fidning thefirst occurence
+        low = mid + 1; //finding the last occurence
+    }
+  }
+
+3. Count occurrences of a number in a sorted array with duplicates using Binary Search
+
+  Technique : as mentionedinr above problem find the first and last occurence of that no. in array.
+  time complexity : O(log(n))
+  space complexity: O(1)
+    
+4. How many times is a sorted array rotated?
+    
+    Technique : using linear search
+    time complexity : O(n)
+    space complexity: O(1)
+      
+      
+     Technique : using Binary search
+    time complexity : O(log(n))
+    space complexity: O(1)
+      
+      
+ 
+ 
